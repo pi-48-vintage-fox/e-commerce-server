36baa498b5c81b4d80a382fc1929ac8377df7f12
Bimo dwien Prabowo <bimodwienprabowo@Bimos-MacBook-Air.local>
2020-11-10 01:25:22 +0700

    update: product test ontheway

---
+++ server/middlewares/auth.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const { User } = require('../models/index')
+const { verifyToken } = require('../helpers/jwt')
+
+async function authentication(req, res, next) {
+  try {
+    const access_token = req.headers.access_token
+    if(!access_token){
+      throw { msg : 'unauthorized', status : 401 }
+    }
+    const decoded = verifyToken(access_token)
+    const user = await User.findOne({
+      where : {
+        email : decoded.email
+      }
+    })
+    if (!user){
+      throw { msg : 'unathorized', status : 401 }
+    }
+    else {
+      req.loginUser = user
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
+}
+
+async function authorization(req, res, next) {
+  const id = req.loginUser.id
+  try {
+    const user = await User.findByPk(id)
+    if (user.role === "Admin"){
+      next()
+    }
+    else {
+      throw { msg : "Ente bukan admin bos", status : 401}
+    }
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = {
+  authentication,
+  authorization
+}
--- server/tests/product.test.js
+++ server/tests/product.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest')
 const app = require('../app')
 
-// Failed
-// Create
+
+// Create Failed
+
 describe("Test failed CRUD products", () => {
-  it("Test failed without access_token", () => {
+  it("Test failed without access_token", (done) => {
     request(app)
     .post('/products')
     .send({
@@ -26,5 +27,209 @@ describe("Test failed CRUD products", () => {
 })
 
 describe("Test failed CRUD Products", () => {
-  it()
+  it("Test failed with empty field", (done) => {
+    request(app)
+    .post('/products')
+    .send({
+      name : "",
+      image_url : "",
+      price : 500000,
+      stock : 2
+    })
+    .set("access_token", access_token)
+    .then((response) => {
+      let { body, status } = response
+      expect(status).toBe(400)
+      expect(body).toHaveProperty(Object.keys(response.body))
+      done()
+    })
+    .catch(err => {
+      done()
+    })
+  })
+})
+
+describe("Test failed CRUD Products", () => {
+  it("Test failed with stock minus", (done) => {
+    request(app)
+    .post('/products')
+    .send({
+      name : "Sepatu Nike",
+      image_url : "https://awsimages.detik.net.id/community/media/visual/2019/01/17/f31f05dd-5e6b-42f7-969d-c03ab4540729_169.jpeg?w=620",
+      price : 1000000,
+      stock : -1
+    })
+    .set("access_token", access_token)
+    .then((response) => {
+      let { body, status } = response
+      expect(status).toBe(400)
+      expect(body).toHaveProperty(Object.keys(response.body))
+      done()
+    })
+    .catch(err => {
+      done()
+    })
+  })
+})
+
+describe("Test failed CRUD Products", () => {
+  it("test failed with price minus", (done) => {
+    request(app)
+    .post('/products')
+    .send({
+      name : "Sepatu Nike",
+      image_url : "https://awsimages.detik.net.id/community/media/visual/2019/01/17/f31f05dd-5e6b-42f7-969d-c03ab4540729_169.jpeg?w=620",
+      price : -1000000,
+      stock : 10
+    })
+    .then((response) => {
+      let { body, status } = response
+      expect(status).toBe(400)
+      expect(body).toHaveProperty(Object.keys(response.body))
+      done()
+    })
+    .catch(err => {
+      done()
+    })
+  })
+})
+
+describe("Test failed CRUD Products", () => {
+  it("test failed with different types", (done) => {
+    request(app)
+    .post('/products')
+    .send({
+      name : "Sepatu Nike",
+      image_url : "https://awsimages.detik.net.id/community/media/visual/2019/01/17/f31f05dd-5e6b-42f7-969d-c03ab4540729_169.jpeg?w=620",
+      price : 1000000,
+      stock : "integer"
+    })
+    .set("access_token", access_token)
+    .then((response) => {
+      let { body, status } = response
+      expect(status).toBe(400)
+      expect(body).toHaveProperty(Object.keys(response.body))
+      done()
+    })
+    .catch(err => {
+      done()
+    })
+  })
+})
+
+//Update Failed
+
+describe("Test failed CRUD Products", () => {
+  it("test failed without access_token", (done) => {
+    request(app)
+    .put('/products/2')
+    .send({
+      name : "Sepatu Adidas",
+      image_url : "https://ecs7.tokopedia.net/img/cache/700/product-1/2018/12/3/4887182/4887182_5b9f9a5f-0a9c-4aae-8e73-4e99dd6d997d_700_700.jpg.webp",
+      price : 1000000,
+      stock : 5
+    })
+    .then((response) => {
+      let { body, status } = response
+      expect(status).toBe(400)
+      expect(body).toHaveProperty(Object.keys(response.body))
+      done()
+    })
+    .catch(err => {
+      done()
+    })
+  })
+})
+
+describe("Test failed CRUD Products", () => {
+  it("test failed with minus stock", (done) => {
+    response(app)
+    .put('/products/2')
+    .send({
+      name : "Sepatu Adidas",
+      image_url : "https://ecs7.tokopedia.net/img/cache/700/product-1/2018/12/3/4887182/4887182_5b9f9a5f-0a9c-4aae-8e73-4e99dd6d997d_700_700.jpg.webp",
+      price : 1000000,
+      stock : -5
+    })
+    .set("access_token", access_token)
+    .then((response) => {
+      let { body, status } = response
+      expect(status).toBe(400)
+      expect(body).toHaveProperty(Object.keys(response.body))
+      done()
+    })
+    .catch(err => {
+      done()
+    })
+  })
+})
+
+describe("Test failed CRUD Products", () => {
+  it("test failed with minus price", (done) => {
+    response(app)
+    .put('/products/2')
+    .send({
+      name : "Sepatu Adidas",
+      image_url : "https://ecs7.tokopedia.net/img/cache/700/product-1/2018/12/3/4887182/4887182_5b9f9a5f-0a9c-4aae-8e73-4e99dd6d997d_700_700.jpg.webp",
+      price : -1000000,
+      stock : 5
+    })
+    .set("access_token", access_token)
+    .then((response) => {
+      let { body, status } = response
+      expect(status).toBe(400)
+      expect(body).toHaveProperty(Object.keys(response.body))
+      done()
+    })
+    .catch(err => {
+      done()
+    })
+  })
+})
+
+describe("Test failed CRUD Products", () => {
+  it("test failed with different types", (done) => {
+    request(app)
+    .put('/products')
+    .send({
+      name : "Sepatu Nike",
+      image_url : "https://awsimages.detik.net.id/community/media/visual/2019/01/17/f31f05dd-5e6b-42f7-969d-c03ab4540729_169.jpeg?w=620",
+      price : 1000000,
+      stock : "integer"
+    })
+    .set("access_token", access_token)
+    .then((response) => {
+      let { body, status } = response
+      expect(status).toBe(400)
+      expect(body).toHaveProperty(Object.keys(response.body))
+      done()
+    })
+    .catch(err => {
+      done()
+    })
+  })
+})
+
+// Delete Failed
+
+describe("test failed CRUD Products", () => {
+  it("test failed without access_token", (done) => {
+    request(app)
+    .delete('/products/2')
+    .send({
+      name : "Sepatu Adidas",
+      image_url : "https://ecs7.tokopedia.net/img/cache/700/product-1/2018/12/3/4887182/4887182_5b9f9a5f-0a9c-4aae-8e73-4e99dd6d997d_700_700.jpg.webp",
+      price : 1000000,
+      stock : 5
+    })
+    .then((response) => {
+      let {body, status} = response
+      expect(status).toBe(401)
+      expect(body).toHaveProperty(Object.keys(response.body))
+      done()
+    })
+    .catch(err => {
+      done()
+    })
+  })
 })
