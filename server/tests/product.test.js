09b3565be50cf8fb7d2f2f641463e13f45ec406c
Bimo dwien Prabowo <bimodwienprabowo@Bimos-MacBook-Air.local>
2020-11-10 02:23:04 +0700

    update: product test

--- server/tests/product.test.js
+++ server/tests/product.test.js
@@ -6,6 +6,94 @@ const { signToken } = require('../helpers/jwt')
 const { queryInterface } = sequelize
 
 
+// Success Create CRUD
+
+describe("Test Success Crud products", () => {
+  it("Test success Post Products", (done) => {
+    request(app)
+    .get('/products')
+    .send({
+      name : "Sepatu Nike",
+      image_url : "https://awsimages.detik.net.id/community/media/visual/2019/01/17/f31f05dd-5e6b-42f7-969d-c03ab4540729_169.jpeg?w=620",
+      price : 1000000,
+      stock : 5
+    })
+    .set('access_token', access_token)
+    .then((response) => {
+      let { body, status } = response
+      expect(status).toBe(200)
+      expect(response).toHaveProperty("body", expect.any(Object))
+      done()
+    })
+    .catch(err => {
+      done()
+    })
+  })
+})
+
+describe("Test Success Crud products", () => {
+  it("Test success Post Products", (done) => {
+    request(app)
+    .post('/products')
+    .send({
+      name : "Sepatu Nike",
+      image_url : "https://awsimages.detik.net.id/community/media/visual/2019/01/17/f31f05dd-5e6b-42f7-969d-c03ab4540729_169.jpeg?w=620",
+      price : 1000000,
+      stock : 5
+    })
+    .set('access_token', access_token)
+    .then((response) => {
+      let { body, status } = response
+      expect(status).toBe(200)
+      expect(response).toHaveProperty("body", expect.any(Object))
+      done()
+    })
+    .catch(err => {
+      done()
+    })
+  })
+})
+
+describe("Test Success Crud products", () => {
+  it("Test success Post Products", (done) => {
+    request(app)
+    .put('/products/2')
+    .send({
+      name : "Sepatu Nike",
+      image_url : "https://awsimages.detik.net.id/community/media/visual/2019/01/17/f31f05dd-5e6b-42f7-969d-c03ab4540729_169.jpeg?w=620",
+      price : 1000000,
+      stock : 5
+    })
+    .set('access_token', access_token)
+    .then((response) => {
+      let { body, status } = response
+      expect(status).toBe(200)
+      expect(response).toHaveProperty("body", expect.any(Object))
+      done()
+    })
+    .catch(err => {
+      done()
+    })
+  })
+})
+
+describe("Test Success Crud products", () => {
+  it("Test success Post Products", (done) => {
+    request(app)
+    .delete('/products')
+    .set('access_token', access_token)
+    .then((response) => {
+      let { body, status } = response
+      expect(status).toBe(200)
+      expect(response).toHaveProperty("body", expect.any(Object))
+      done()
+    })
+    .catch(err => {
+      done()
+    })
+  })
+})
+
 // Create Failed
 
 describe("Test failed CRUD products", () => {
