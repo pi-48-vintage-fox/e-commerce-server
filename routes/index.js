7797c01de0bc0a6fb13d22cce99db403d1022097
musabagja <bagja7musa@gmail.com>
2020-11-14 18:13:47 +0700

    Deploy fix

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const productsRoutes = require('./product.js');
 const categoriesRoutes = require('./category.js');
 const bannersRoutes = require('./banner.js');
 
+router.get('/', (req, res, next) => {
+  res.send('IM ACTIVE')
+})
+
 router.post('/adminRegister', AdminController.register);
 router.post('/adminLogin', AdminController.login);
 
