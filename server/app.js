3554353eab46887bfda0524e465cd5258bf4eba7
ikhromwicaksono <wicaksono.ikram26@gmail.com>
2020-11-14 13:29:11 +0700

    ubah posisi-cors

--- server/app.js
+++ server/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -6,15 +5,15 @@ const routes = require('./routes')
 const PORT = process.env.PORT || 3000
 const errorHandler = require('./middlewares/errorhandler')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
 
 app.use(routes)
 app.use(errorHandler)
 
-app.listen(PORT, () => {
-  console.log('app is running');
-})
+// app.listen(PORT, () => {
+//   console.log('app is running');
+// })
 
 module.exports = app
