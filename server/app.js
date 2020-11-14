48407f159695eec5148ad86da3db7647f80e293a
AbdulRasyid-Ans <abdul.rasyi.anshori@gmail.com>
2020-11-14 17:43:08 +0700

    deploy

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
-require('dotenv').config()
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config() 
+} 
 const port = process.env.PORT || 3000
 const routes = require("./routes")
 const cors = require('cors')
