8e81cfda2d26889a50250f7fb6c8e60ae2e27103
nandahasyim-mar <nandahasyim@gmail.com>
2020-11-14 19:51:58 +0700

    Haha

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router()
 const productRouter = require('./productRouter')
 // const bannerRouter = require('./bannerRouter')
 const userController = require('../controllers/userControllers')
-const authentication = require('../middleware/authentication')
+// const authentication = require('../middleware/authentication')
 
 router.post('/login', userController.login)
-router.use(authentication)
+// router.use(authentication)
 router.use('/products', productRouter)
 // router.use(bannerRouter)
 
