e3b46fd641db094cd655ae6b5ba11e2503c3c7b1
dakochan666 <adrianmuhammadkuncoro@gmail.com>
2020-11-14 17:38:34 +0700

    heroku

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config()
+}
 const express = require('express');
 const cors = require('cors')
 const app = express()
 const routes = require('./routes/index')
 const errorHandler = require('./middlewares/errorHandler');
-require('dotenv').config()
 const port = 3000 || process.env.PORT
 
 app.use(cors())
