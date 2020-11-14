70a24b16eef33faebd5c3645f40cb2f65cc5a8d3
Haris Pratama <hpratama324@gmail.com>
2020-11-14 12:55:06 +0700

    deploy heroku4

---
+++ seeders/20201114055217-seed-user.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [{
+      email: 'admin',
+      password: '301197',
+      role: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('People', null, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
