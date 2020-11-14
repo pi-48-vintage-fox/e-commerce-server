d65393dec05c3ad4599af13b956fb138f0b60742
daryalfuad <daryalfuad@gmail.com>
2020-11-14 09:01:40 +0700

    lupa cors

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express()
 const port = 3000
 const router = require('./routes/index')
 const errorHandler = require('./middlewares/errorHandler')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/', router)
