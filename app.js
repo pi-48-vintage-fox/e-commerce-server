02dcce8989f051d77669050e729262046e1c1a43
dakochan666 <adrianmuhammadkuncoro@gmail.com>
2020-11-14 17:23:39 +0700

    heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const routes = require('./routes/index')
 const errorHandler = require('./middlewares/errorHandler');
 require('dotenv').config()
-const port = 3000
+const port = 3000 || process.env.PORT
 
 app.use(cors())
 app.use(express.json())
