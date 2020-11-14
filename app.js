ba2e3295430ac7910e966c8c432dc4c88cb240da
Haris Pratama <hpratama324@gmail.com>
2020-11-14 12:48:54 +0700

    deploy heroku3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const router = require('./routes/index');
 const cors = require('cors');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
