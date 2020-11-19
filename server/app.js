857ba7f75cbde4ab7dc24cbfbf12df95e2da304c
ikhromwicaksono <wicaksono.ikram26@gmail.com>
2020-11-19 14:40:29 +0700

    test lagi

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const routes = require('./routes')
-//const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 const errorHandler = require('./middlewares/errorhandler')
 
 app.use(cors())
