535a4d9497c5a3251c52db90fefca34c5f107ec8
rivari22 <muchammadrvr22@gmail.com>
2020-11-19 12:09:56 +0700

    deploy

--- seeders/20201111091317-seed-orders.js
+++ seeders/20201111091317-seed-orders.js
@@ -11,44 +11,44 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
      */
-    const data = [
-      {
-        UserId: 1,
-        ProductId: 1,
-        status: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        UserId: 1,
-        ProductId: 3,
-        status: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        UserId: 2,
-        ProductId: 1,
-        status: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        UserId: 2,
-        ProductId: 2,
-        status: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        UserId: 2,
-        ProductId: 3,
-        status: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-    ];
-    queryInterface.bulkInsert("Orders", data)
+    // const data = [
+    //   {
+    //     UserId: 1,
+    //     ProductId: 1,
+    //     status: false,
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    //   },
+    //   {
+    //     UserId: 1,
+    //     ProductId: 3,
+    //     status: false,
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    //   },
+    //   {
+    //     UserId: 2,
+    //     ProductId: 1,
+    //     status: false,
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    //   },
+    //   {
+    //     UserId: 2,
+    //     ProductId: 2,
+    //     status: false,
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    //   },
+    //   {
+    //     UserId: 2,
+    //     ProductId: 3,
+    //     status: false,
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    //   },
+    // ];
+    // queryInterface.bulkInsert("Orders", data)
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -58,7 +58,7 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-    queryInterface.bulkDelete("Orders", null)
+    // queryInterface.bulkDelete("Orders", null)
 
   },
 };
